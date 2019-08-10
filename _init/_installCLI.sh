4cb20929f8ae98b3230e73b00456bab7bfa6dd06
fernfernfern <fernandomouco@gmail.com>
2019-08-10 11:27:50 +0100

    this is fine

--- _init/_installCLI.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-_installCLI() {
-    curl -fLSs https://circle.ci/cli | sudo bash && circleci setup
-    sleep 1
-    echo
-    CCI_TOKEN=$(grep -Po "(?<=token: ).*" "$HOME"/.circleci/cli.yml)
-}
